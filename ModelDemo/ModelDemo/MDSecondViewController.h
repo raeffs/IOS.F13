b6b4dbcf5cdcdb08cd48c518a359bf4993db0f6c
Raphael Fleischlin <git@raefftec.ch>
2013-03-08 22:14:34 +0100

    created second view and linked a button

---
+++ ModelDemo/ModelDemo/MDSecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  MDSecondViewController.h
+//  ModelDemo
+//
+//  Created by Raphael Fleischlin on 08.03.13.
+//  Copyright (c) 2013 Raphael Fleischlin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MDSecondViewController : UIViewController
+
+- (IBAction)backPressed:(UIButton*)sender;
+
+@end
---
+++ ModelDemo/ModelDemo/MDSecondViewController.m
@@ -0,0 +1,42 @@
+//
+//  MDSecondViewController.m
+//  ModelDemo
+//
+//  Created by Raphael Fleischlin on 08.03.13.
+//  Copyright (c) 2013 Raphael Fleischlin. All rights reserved.
+//
+
+#import "MDSecondViewController.h"
+
+@interface MDSecondViewController ()
+
+@end
+
+@implementation MDSecondViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)backPressed:(UIButton*)sender
+{
+    
+}
+@end
