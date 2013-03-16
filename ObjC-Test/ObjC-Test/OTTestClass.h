90772aaa2a58b137d1cb3e4ebc88793ad96ec54a
Raphael Fleischlin <git@raefftec.ch>
2013-03-16 10:13:32 +0100

    added a test class

---
+++ ObjC-Test/ObjC-Test/OTTestClass.h
@@ -0,0 +1,19 @@
+//
+//  OTTestClass.h
+//  ObjC-Test
+//
+//  Created by Raphael Fleischlin on 16.03.13.
+//  Copyright (c) 2013 Raphael Fleischlin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OTTestClass : NSObject
+
+@property int i;
+
+@property float f;
+
+@property (retain) NSArray *array;
+
+@end
---
+++ ObjC-Test/ObjC-Test/OTTestClass.m
@@ -0,0 +1,29 @@
+//
+//  OTTestClass.m
+//  ObjC-Test
+//
+//  Created by Raphael Fleischlin on 16.03.13.
+//  Copyright (c) 2013 Raphael Fleischlin. All rights reserved.
+//
+
+#import "OTTestClass.h"
+
+@implementation OTTestClass
+
+- (id) init
+{
+    if (self = [super init])
+    {
+        self.array = [[NSArray alloc] initWithObjects:@"", nil];
+        [self.array release];
+    }
+    return self;
+}
+
+- (void) dealloc
+{
+    self.array = nil;
+    [super dealloc];
+}
+
+@end
